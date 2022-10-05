64c5cc909107c0b12cbefd9f5f77a14a64080b0a
Aaruni Aggarwal <aaruniagg@gmail.com>
2022-10-06 01:20:33 +0530

    fixing error

--- vars/clusterLoggingExportVars.groovy
+++ vars/clusterLoggingExportVars.groovy
@@ -9,7 +9,6 @@ def call(){
                 ssh -o 'StrictHostKeyChecking no' -i id_rsa root@${OCP_IP} "rm -rf /root/ocp_ansible_validation; git clone https://satwsin1:${GITHUBTOKEN}@github.ibm.com/redstack-power/ocp_ansible_validation.git"
 
                 scp -o 'StrictHostKeyChecking no' -i id_rsa deploy/ocp_cluster_logging_vars.yml root@${OCP_IP}:/root/ocp_ansible_validation/examples/
-                scp -o 'StrictHostKeyChecking no' -i id_rsa logging_vars.sh root@${OCP_IP}:/root/ 
                 scp -o 'StrictHostKeyChecking no' -i id_rsa deploy/cl_inventory root@${OCP_IP}:/root/ocp_ansible_validation/
 
                 scp -o 'StrictHostKeyChecking no' -i id_rsa ${WORKSPACE}/scripts/update-logging-var.sh root@${OCP_IP}:/root/           
